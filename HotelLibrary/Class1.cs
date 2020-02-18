e768cd02df5d051b69f2abe005069d59278eaedf
S571736 <Sondre.Gjesdal@gmail.com>
2020-02-18 14:24:38 +0100

    Redder heile prosjektet

---
+++ HotelLibrary/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace HotelLibrary
+{
+    public enum Size
+    {
+        Single,
+        Double,
+        Triple,
+        King
+    }
+    public class HotelRoom
+    {
+        public int nBeds { get; private set; }
+        public Size size { get; private set; }
+        public int roomId { get; private set; }
+        public HotelRoom(int roomId, Size size, int nBeds)
+        {
+            this.roomId = roomId;
+            this.size = size;
+            this.nBeds = nBeds;
+
+        }
+    }
+
+    public class Booking
+    {
+        public int customerId { get; private set; }
+        public int roomId { get; private set; }
+        public DateTime to { get; private set; }
+        public DateTime from { get; private set; }
+        public string note { get; private set; }
+        public Booking(int customerId, int roomId, DateTime to, DateTime from, string note)
+        {
+
+        }
+    }
+
+}
