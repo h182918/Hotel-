a043d69deb51385e89cd2a757fccf7a00155fee0
Eli Vissvass <elisabethvossgaard@gmail.com>
2020-02-19 12:32:58 +0100

    hei

---
+++ WebAppClient/CreateBooking/CreateBooking.aspx.cs
@@ -0,0 +1,62 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Web;
+//using System.Web.UI;
+//using System.Web.UI.WebControls;
+
+//namespace WebAppClient.CreateBooking
+//{
+//    public partial class CreateBooking : System.Web.UI.Page
+//    {
+
+//        List<Booking> bookings;
+//        List<HotelRoom> rooms;
+
+//        protected void Page_Load(object sender, EventArgs e)
+//        {
+//            foreach(HotelRoom r in rooms)
+//            {
+//                if(r.roomId )
+//            }
+//            //set date
+
+
+
+//        }
+
+//        protected void FindRooms(object sender, EventArgs e)
+//        {
+       
+
+
+//            Response.Redirect("CreateBooking.aspx");
+//        }
+
+//        protected bool AvailableRoom(DateTime dateFrom, DateTime dateTo, int nBeds, Enum.Size size)
+//        {
+//            var validItems = items.Where(i => i.Field != null && i.State != ItemStates.Deleted);
+//            // command
+//            foreach (var item in validItems)
+//            {
+//                // do stuff
+
+//                List<HotelRoom> validRooms= rooms.Where(i => i.nbeds == nBeds && i.size.Equals(size));
+
+
+//                //loop through rooms 
+//                foreach(HotelRoom r in validRooms)
+//                {
+//                    if()
+//                }
+//            {
+//                if(rooms.)
+//                {
+//                    return false;
+//                }
+//            }
+
+//        }
+
+//    }
+//}
