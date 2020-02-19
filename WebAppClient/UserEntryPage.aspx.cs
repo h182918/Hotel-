90100da44e10585b4d28005f27d2ee6f9ae9b4c4
Eli Vissvass <elisabethvossgaard@gmail.com>
2020-02-19 10:36:46 +0100

    hei

--- WebAppClient/UserEntryPage.aspx.cs
+++ WebAppClient/UserEntryPage.aspx.cs
@@ -1,16 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
+﻿using HotelLibrary;
+using System;
 
 namespace WebAppClient
 {
     public partial class UserEntryPage : System.Web.UI.Page
     {
 
-        //List<Booking> bookings;
+        List<Booking> bookings;
         protected void Page_Load(object sender, EventArgs e)
         {
             //bookings = DB.getbookings(userID)
